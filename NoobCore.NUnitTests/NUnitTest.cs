ab9e5306e55f98cb354706b56e8ff48d3a348570
guoguo <noobwu@126.com>
2017-10-08 18:10:37 +0800

    NUnit

---
+++ NoobCore.NUnitTests/NUnitTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnit.Framework;
+namespace NoobCore.NUnitTests
+{
+    public class NUnitTest
+    {
+        [TestCase]
+        public void PassTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
